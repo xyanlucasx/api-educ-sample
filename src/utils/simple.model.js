700d5f4ee73b96e9e19443fb92b5ca057bc3e33c
Yan Lucas <xyan.lucasx@live.com>
2021-08-01 20:26:32 -0300

    feat: improve simple model system

--- src/utils/simple.model.js
+++ src/utils/simple.model.js
@@ -1,11 +1,17 @@
-export default (data, fields) => {
+export default (data, fields, noRequired) => {
   const allowFields = fields;
 
   const validFields = allowFields.reduce((acc, curr) => {
-    if (data[curr]) acc[curr] = data[curr];
+    if (data[curr] || noRequired) acc[curr] = data[curr];
     else throw new Error(`field '${curr}' is required`);
     return acc;
   }, {});
+
+  if (noRequired) {
+    Object.keys(validFields).map((field) => {
+      if (!validFields[field]) delete validFields[field];
+    });
+  }
 
   return validFields;
 };
