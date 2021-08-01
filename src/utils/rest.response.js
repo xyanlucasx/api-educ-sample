af4ac3f70bcf26bb0cc01ec687971d7e28b05f16
Yan Lucas <xyan.lucasx@live.com>
2021-08-01 18:58:37 -0300

    feat: add new http responses in rest response file

--- src/utils/rest.response.js
+++ src/utils/rest.response.js
@@ -1,25 +1,26 @@
 export const ok = (res, data) => {
-    res.status(200).send(data)
-}
+  res.status(200).send(data);
+};
 
-export const created = (res, data) => {
-
-}
+export const responseCreated = (res, data) => {
+  res.status(201).send(data);
+};
 export const noContent = (res) => {
-    res.status(204).send()
-}
-export const notFound = (res) => {
+  res.status(204).send();
+};
+export const notFound = (res) => {};
+export const unauthorized = (res) => {};
+export const forbidden = (res) => {};
 
-}
-export const unauthorized = (res) => {
+export const responseBadRequest = (res, customError) => {
+  res.status(400).send({ message: customError ? customError : "Bad request" });
+};
 
-}
-export const forbidden = (res) => {
-
-}
 export const internalServerError = (res, customError) => {
-    res.status(500).send({ message: customError ? customError : 'Internal error' })
-}
+  res
+    .status(500)
+    .send({ message: customError ? customError : "Internal error" });
+};
 
 //export default {
 //    ok, created, noContent, notFound, unauthorized, forbidden, internalServerError
